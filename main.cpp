c60d99a288c8045433ac389f7a567f42ff06a60e
cjdyer <cjdyer0@gmail.com>
2021-06-15 20:01:01 +1200

    Basic movement testing
    This will control a character at a base level.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int x, y;
+bool running = true; // is game running 
+
+void registerKeyPress()
+{
+    if(_kbhit()) // If key hit
+    {
+        switch(_getch()) // switch on key
+        {
+        case 'w': //up
+            y++;
+            break;
+        case 'a': //left
+            x--;
+            break;
+        case 's': //down
+            y--;
+            break;
+        case 'd': //right
+            x++;
+            break;
+        default: // Anything else pressed
+            running = false;
+            break;
+        }
+    }
+}
+
+void drawGame()
+{
+    system("CLS");
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
+}
 
 int main()
 {
-    
+    while(running)
+    {
+        registerKeyPress();
+        drawGame();
+    }
 }
